b2dba5893f3ec0a8ddb80ae969c4d38a5854e5a7
bkmsstudio <51231559+bkmsstudio@users.noreply.github.com>
2019-06-29 02:52:34 +0700

    Add files via upload

---
+++ ServerCore/Premake5Build.lua
@@ -0,0 +1,25 @@
+project "ServerCore"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	location (path.join(repoRoot, "Build/%{prj.name}"))
+	targetdir (path.join(repoRoot, "Bin/%{cfg.platform}/%{cfg.buildcfg}"))
+
+	includedirs {
+		-- SAMP-EDGEngine
+		path.join(repoRoot, "Engine/include"),
+
+		-- SAMPGDK
+		path.join(userConfig.deps.sampgdk.root, "include"),
+		
+		-- SAMP Plugin SDK
+		userConfig.deps.samp_plugin_sdk.root,
+		path.join(userConfig.deps.samp_plugin_sdk.root, "amx"),
+		
+		-- QuickMaffs
+		path.join(userConfig.deps.quickmaffs.root, "include")
+	}
+	
+	files {
+		"src/ServerCore.cpp"
+	}
