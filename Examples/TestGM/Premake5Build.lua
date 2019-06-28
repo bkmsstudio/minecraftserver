8861de86b5ab296e1d9cefad8788872b1379ba66
bkmsstudio <51231559+bkmsstudio@users.noreply.github.com>
2019-06-29 02:50:14 +0700

    Add files via upload

---
+++ Examples/TestGM/Premake5Build.lua
@@ -0,0 +1,58 @@
+if userConfig.build.examples and not userConfig.build.extensions then
+	printf("TestGM example requires to build extensions. Skipping...")
+else
+	project "TestGM"
+		kind "SharedLib"
+		language "C++"
+		cppdialect "C++17"
+		location (path.join(repoRoot, "Build/Examples/%{prj.name}"))
+		targetdir (path.join(repoRoot, "Bin/%{cfg.platform}/%{cfg.buildcfg}/Examples"))
+
+		-- Use .def file with Visual Studio compiler.
+		if string.match(_ACTION, "vs%d%d%d%d") ~= nil then
+			linkoptions { "/DEF:\"" .. path.join(repoRoot, "SymbolsExportList.def") .. "\""}
+		end
+
+		includedirs {
+			-- Ext_ResourceIO:
+			path.join(repoRoot, "Extensions/ResourceIO/include"),
+
+			-- SAMP-EDGEngine:
+			path.join(repoRoot, "Engine/include"),
+
+			-- SAMPGDK:
+			path.join(userConfig.deps.sampgdk.root, "include"),
+			
+			-- QuickMaffs:
+			path.join(userConfig.deps.quickmaffs.root, "include"),
+
+			-- RapidXML:
+			userConfig.deps.rapidxml.root
+		}
+
+		-- gmake adds ServerCore.cpp directly (TODO: change this.)
+		if _ACTION == "gmake" then
+			files { path.join(repoRoot, "ServerCore/src/ServerCore.cpp") }
+			links {
+				"Ext_ResourceIO",
+				"Engine",
+				-- SAMPGDK:
+				path.join(userConfig.deps.sampgdk.root, "lib/%{cfg.platform}/%{cfg.buildcfg}/sampgdk")
+			}
+			buildoptions { "-Wno-attributes" }
+		else
+			links {
+				"Ext_ResourceIO",
+				"ServerCore",
+				"Engine",
+				-- SAMPGDK:
+				path.join(userConfig.deps.sampgdk.root, "lib/%{cfg.platform}/%{cfg.buildcfg}/sampgdk")
+			}
+		end
+
+		edge.useSampPluginSdk(userConfig.deps.samp_plugin_sdk.root)
+
+		files {
+			"src/Main.cpp",
+		}
+end
---
+++ Examples/TestGM/src/Main.cpp
@@ -0,0 +1,141 @@
+
+#include <SAMP-EDGEngine/Everything.hpp>
+#include <SAMP-EDGEngine/Ext/ResourceIO/Everything.hpp>
+#include <iostream>
+#include <fstream>
+
+#include <filesystem>
+namespace fs = std::filesystem;
+
+namespace samp = samp_edgengine;
+namespace rcio = samp_edgengine::ext::resource_io;
+
+void cmd_TeleportToLocation(samp::CommandInput input_)
+{
+	auto params = input_.splitParams();
+	if (params.size() >= 3)
+	{
+		samp::math::Vector3f location;
+		location.x = samp::text::convert<float>(params[0]).value();
+		location.y = samp::text::convert<float>(params[1]).value();
+		location.z = samp::text::convert<float>(params[2]).value();
+		input_.target.setLocation(location);
+		if (params.size() >= 5)
+		{
+			std::int32_t world		= samp::text::convert<std::int32_t>(params[3]).value();
+			std::int32_t interior	= samp::text::convert<std::int32_t>(params[4]).value();
+			input_.target.setWorld(world);
+			input_.target.setInterior(interior);
+		}
+	}
+}
+
+void cmd_SpawnVehicle(samp::CommandInput input_)
+{
+	auto params = input_.splitParams();
+	if (params.size() == 1)
+	{
+		auto pVeh = GameMode->map.beginConstruction<samp::Vehicle>();
+		pVeh->setModel(samp::Vehicle::findModelBestMatch(params[0]));
+		pVeh->setLocation(input_.target.getLocation());
+		pVeh->setFacingAngle(input_.target.getFacingAngle());
+		pVeh->setInterior(input_.target.getInterior());
+		pVeh->setWorld(input_.target.getWorld());
+		auto &veh = GameMode->map.finalizeConstruction(pVeh);
+		input_.target.putInVehicle(veh, 0);
+	}
+}
+
+void cmd_ObjectCount(samp::CommandInput input_)
+{
+	namespace txt = samp::text;
+	auto tracker = static_cast<samp::default_streamer::PlayerWrapper*>(input_.target.getPlacementTracker());
+	GameMode->chat->messagePlayer(input_.target, txt::compose("You have currently ", tracker->spawnedObjects.size(), " objects shown!"));
+}
+
+class MyGameMode
+	: public samp::IGameMode
+{
+public:
+	MyGameMode() {	
+		this->addPlayerClass(0, { 280, 180, 1010 }, 0, { samp::Weapon(samp::Weapon::Deagle, 999) });
+	}
+	
+	virtual void setup() override
+	{
+		samp::IGameMode::setup();
+
+		chat = std::make_unique<samp::DefaultChat>(*this);
+
+		this->setupCommands();
+		this->loadObjects();
+	}
+
+	void loadObjects()
+	{
+		for (auto& file : fs::directory_iterator("data/objects"))
+		{
+			if (fs::is_regular_file(file))
+			{
+				std::clog << "[i]: Reading file: " << file.path() << std::endl;
+
+				// Open file:
+				std::ifstream inputStream{ file.path().string() };
+
+				auto scene = GameMode->map.beginConstruction<samp::Scene>();
+
+				// Create XML document:
+				rcio::xml::xml_document<> document;
+
+				// Read entire file to the string:
+				std::string inputContent;
+				{
+					char buffer[4 * 1024];
+					while (inputStream.read(buffer, sizeof(buffer)))
+						inputContent.append(buffer, sizeof(buffer));
+					inputContent.append(buffer, inputStream.gcount());
+				}
+				// Make sure string is null-terminated:
+				inputContent.c_str();
+				// Parse the document:
+				document.parse<0>(inputContent.data());
+
+				if (auto rootNode = document.first_node("MapScene"))
+				{
+					// Deserialize the scene:
+					rcio::XMLSceneDeserializer deserializer{ *scene, *rootNode };
+					if (deserializer.deserialize())
+					{
+						GameMode->map.finalizeConstruction(scene);
+						std::cout << "Read: " << scene->getObjects().size() << " objects!" << std::endl;
+					}
+				}
+				
+			}
+		}
+
+	}
+
+	virtual void setupEvents() override {
+		Server->onPlayerSpawn += { *this, &MyGameMode::whenPlayerSpawns };
+	}
+
+	void whenPlayerSpawns(samp::Player & player_)
+	{
+		player_.setLocation({ 1958.33f, 1343.12f, 15.36f });
+		player_.setWorld(0);
+		player_.addWeapon(samp::Weapon(samp::Weapon::Deagle, 999));
+	}
+
+	void setupCommands()
+	{
+		commands.construct<samp::ProcedureCommand>( samp::CmdInvocations{ "tpc" }, cmd_TeleportToLocation );
+		commands.construct<samp::ProcedureCommand>( samp::CmdInvocations{ "v" }, cmd_SpawnVehicle );
+		commands.construct<samp::ProcedureCommand>( samp::CmdInvocations{ "objc" }, cmd_ObjectCount);
+	}
+};
+
+samp::GameModeSetupResult SAMPGameModeSetup()
+{
+	return std::make_unique<MyGameMode>();
+}
